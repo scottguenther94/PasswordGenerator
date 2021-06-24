28e9bdb39da910e64fe33019f5d97feea20b5b00
Southern Comfort <scottguenther94@gmail.com>
2021-06-24 00:07:27 -0500

    Added number range, changed labels

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 //Code copied, pasted, and modified from W3 Collective
 
 //Characters to be allowed in Password Generator 
-const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numbers = "0123456789";
-const symbols = "!@#$%^&*_-+=";
+var alpha = "abcdefghijklmnopqrstuvwxyz";
+var upper ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = []
+for (var i = 1; i <= 128; i++) {
+    numbers.push(i)
+}
+var symbols = "!@#$%^&*_-+=";
 
-//
-const passwordTxt = document.getElementById("password");
-const length = document.getElementById("length");
-const incNumbers = document.getElementById("numbers");
-const incSymbols = document.getElementById("symbols");
-const generateBtn = document.getElementById("generate");
+//Declaring variables for DOM elements
+var passwordTxt = document.getElementById("password");
+var length = document.getElementById("length");
+var incUpper = document.getElementById("upper");
+var incNumbers = document.getElementById("numbers");
+var incSymbols = document.getElementById("symbols");
+var generateBtn = document.getElementById("generate");
 
+//Click event for "Generate Password" button
 generateBtn.addEventListener("click", () => {
     let characters = alpha;
+    incUpper.checked ? (characters += upper) : "";
     incNumbers.checked ? (characters += numbers) : "";
     incSymbols.checked ? (characters += symbols) : "";
     passwordTxt.value = generatePassword(length.value, characters);
   });
-  const generatePassword = (length, characters) => {
+
+  //Function that generates the password
+  var generatePassword = (length, characters) => {
     let password = "";
     for (let i = 0; i < length; i++) {
       password += characters.charAt(
@@ -27,7 +36,8 @@ generateBtn.addEventListener("click", () => {
     }
     return password;
   };
-  const copyBtn = document.getElementById("copy");
+  //Copy button function
+  var copyBtn = document.getElementById("copy");
 copyBtn.addEventListener("click", () => {
   passwordTxt.select();
   document.execCommand("copy");
