8a53dc4202baf8c1abe02ef46c42434a65b69e1e
Southern Comfort <scottguenther94@gmail.com>
2021-06-23 23:46:12 -0500

    Updated JS and HTML

--- script.js
+++ script.js
@@ -6,19 +6,20 @@ var symbols = ["!@#$%^&*_-+="];
 
 var passwordTxt = document.getElementById("password");
 var incUpper = document.getElementById("upper");
-var inLower = document.getElementById("lower");
 var incNumbers = document.getElementById("numbers");
 var incSymbols = document.getElementById("symbols");
 var generateBtn = document.getElementById("generate");
 var length = prompt("Enter a number from 8 to 128");
 
+// Button click event, instructions for which characters to use
 generateBtn.addEventListener("click", () => {
-    let characters = alpha;
+    let characters = lowerCharacters;
+    incUpper.checked ? (characters += upperCharacters) : "";
     incNumbers.checked ? (characters += numbers) : "";
     incSymbols.checked ? (characters += symbols) : "";
     passwordTxt.value = generatePassword(length.value, characters);
   });
-
+// For loop to randomly assign characters and generate password
   var generatePassword = (length, characters) => {
     let password = "";
     for (let i = 0; i < length; i++) {
@@ -28,11 +29,10 @@ generateBtn.addEventListener("click", () => {
     }
     return password;
   };
-
+// Copy Password feature
 var copyBtn = document.getElementById("copy");
 copyBtn.addEventListener("click", () => {
   passwordTxt.select();
   document.execCommand("copy");
   alert("Password Copied");
 });
-
