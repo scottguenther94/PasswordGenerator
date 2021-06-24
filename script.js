a1d4dda3b0b1cee103af1349d0ab50fe952e42a7
Southern Comfort <scottguenther94@gmail.com>
2021-06-23 22:45:34 -0500

    Updated JS

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 // Password Generator JS Code
-var generateBtn = document.querySelector("#generate");
+var lowerCharacters = ["abcdefghijklmnopqrstuvwxyz"];
+var upperCharacters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var numbers = ["0123456789"];
+var symbols = ["!@#$%^&*_-+="];
 
-// Write password 
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-    passwordText.value = password;
-}
-function generatePassword() {
-    var lowerCharacters = ["a", "b", "c", "d", "e"];
-    var numCharacters = prompt("Enter a number from 8 to 128");
-    var output = "";
-    console.log(numCharacters);
-    for (let i = 0; i < numCharacters; i++){
-        returnString += lowerCharacters[Math.floor(Math.random() * lowerCharacters.length)]
+var passwordTxt = document.getElementById("password");
+var incUpper = document.getElementById("upper");
+var inLower = document.getElementById("lower");
+var incNumbers = document.getElementById("numbers");
+var incSymbols = document.getElementById("symbols");
+var generateBtn = document.getElementById("generate");
+var length = prompt("Enter a number from 8 to 128");
+
+generateBtn.addEventListener("click", () => {
+    let characters = alpha;
+    incNumbers.checked ? (characters += numbers) : "";
+    incSymbols.checked ? (characters += symbols) : "";
+    passwordTxt.value = generatePassword(length.value, characters);
+  });
+
+  var generatePassword = (length, characters) => {
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      password += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
     }
-    return returnString;
-}
-console.log(generatePassword());
+    return password;
+  };
+
+var copyBtn = document.getElementById("copy");
+copyBtn.addEventListener("click", () => {
+  passwordTxt.select();
+  document.execCommand("copy");
+  alert("Password Copied");
+});
+
